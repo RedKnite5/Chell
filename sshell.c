60f97894e838b75474f7a060d61ecee5c5c38282
RedKnite5 <mr.awesome10000@gmail.com>
2023-01-15 19:39:03 -0800

    change system call to use fork and exec

--- sshell.c
+++ sshell.c
@@ -2,8 +2,20 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+#include <sys/wait.h>
 
 #define CMDLINE_MAX 512
+
+void split_str(char *string, char *array[16]) {
+
+    array[0] = strtok(string, " ");
+    
+    int arg = 1;
+    while(array[arg] != NULL) {
+        array[arg] = strtok(NULL, " ");
+        arg += 1;
+    }
+}
 
 int main(void)
 {
@@ -39,7 +51,28 @@ int main(void)
         }
 
         /* Regular command */
-        retval = system(cmd);
+        char *array[16];
+        char *token;
+        token = strtok(cmd, " ");
+        array[0] = token;
+    
+        int arg = 1;
+        while(token != NULL) {
+            token = strtok(NULL, " ");
+            array[arg] = token;
+            arg += 1;
+        }
+        
+        int status;
+        if (fork()) {
+            waitpid(-1, &status, 0);
+            
+            retval = WEXITSTATUS(status);
+        } else {
+            execvp(array[0], array);
+            exit(1);
+        }
+        
         printf("+ completed '%s' [%d]\n", cmd, retval);
     }
 
